d5427eb4920e11205d72d322ef1440896f53cc7d
GWH <159546902+wahshf@users.noreply.github.com>
2025-05-25 15:14:21 +0300

    661

---
+++ cpp/c501-1000/661.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    public:
+        vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+          int m = img.size();
+          if(m == 0) return {};
+          int n = img[0].size();
+          vector<vector<int>> res(m, vector<int>(n, 0));
+          
+          for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                int sum = 0;
+                int count = 0;
+                for(int x = max(0, i - 1); x <= min(m-1,i+1); x++){
+                    for(int y = max(0, j - 1); y <= min(n-1, j+1); y++){
+                        sum += img[x][y];
+                        count++;
+                    }
+                }
+                res[i][j] = sum / count;
+            }
+          }
+          return res;  
+        }
+    };
