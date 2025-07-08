c62815df192f1081bb8b31f08f1728197ef211ab
GWH <159546902+wahshf@users.noreply.github.com>
2025-07-08 17:00:52 +0300

    865

---
+++ cpp/c501-1000/865.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    public:
+        TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+            return helper(root).first;
+        }
+        pair<TreeNode*, int> helper(TreeNode* node){
+            if(!node) return {nullptr, 0};
+            auto l = helper(node->left);
+            auto r = helper(node->right);
+            if(l.second>r.second) return {l.first, l.second + 1};
+            if(l.second<r.second) return {r.first, r.second + 1};
+            return {node, l.second + 1};
+        }
+    };
