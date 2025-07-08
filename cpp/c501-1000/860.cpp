435f48f312f5e56145cff61a2b66411895df7e5a
GWH <159546902+wahshf@users.noreply.github.com>
2025-07-08 16:06:49 +0300

    860

---
+++ cpp/c501-1000/860.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    public:
+        bool lemonadeChange(vector<int>& bills) {
+            int f = 0, t = 0;
+            for(int b : bills){
+                if(b == 5){
+                    f++;
+                }else if(b == 10){
+                    if(f == 0){
+                        return false;
+                    }
+                    f--;
+                    t++;
+                }else{
+                    if(t > 00 && f > 0){
+                        t--;
+                        f--;
+                    }else if(f >= 3){
+                        f -= 3;
+                    }else{
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    };
