e023e09b6419b0418ebae3b543d35f991003969a
GWH <159546902+wahshf@users.noreply.github.com>
2025-08-01 12:30:22 +0300

    1003

---
+++ cpp/c1001-1500/1003.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        bool isValid(string s) {
+            vector<char> st;
+            for(char c : s){
+                st.push_back(c);
+                if(st.size() >= 3){
+                    int n = st.size();
+                    if(st[n-3] == 'a' && st[n-2] == 'b' && st[n-1] == 'c'){
+                        st.pop_back();
+                        st.pop_back();
+                        st.pop_back();
+                    }
+                }
+    
+            }
+            return st.empty();
+        }
+    };
