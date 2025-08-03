e4d6723673540983e712ae554682bd9e9abc7817
GWH <159546902+wahshf@users.noreply.github.com>
2025-08-03 16:27:57 +0300

    1015

---
+++ cpp/c1001-1500/1015.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public:
+        int smallestRepunitDivByK(int k) {
+            if(k % 2 == 0 || k % 5 == 0) return -1;
+    
+            int r = 0;
+            for(int l = 1; l <= k; l++){
+                r = (r * 10 + 1) % k;
+                if(r == 0) return l;
+            }
+            return -1;
+        }
+    };
