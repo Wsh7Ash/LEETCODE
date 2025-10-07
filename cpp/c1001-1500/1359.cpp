55c7d29ef347bac944bcee13204a54f573850053
GWH <159546902+wahshf@users.noreply.github.com>
2025-10-07 15:52:47 +0300

    1359

---
+++ cpp/c1001-1500/1359.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public:
+        int countOrders(int n) {
+            const int MOD = 1e9 + 7;
+            long long res = 1;
+            for(int i = 1; i <= n; i++){
+                res = (res * i) % MOD;
+                res = (res * (2 * i - 1)) % MOD;
+            }
+    
+            return (int)res;
+        }
+    };
