d4bd0495253d8397c197fb15c2785591c336b0b4
GWH <159546902+wahshf@users.noreply.github.com>
2025-10-09 14:47:16 +0300

    1374

---
+++ cpp/c1001-1500/1374.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        string generateTheString(int n) {
+            string res = "";
+    
+            if(n % 2 == 1){
+                for(int i = 0; i < n; i++){
+                    res += 'a';
+                }
+            }else{
+                for(int i = 0; i < n - 1; i++){
+                    res += 'a';
+                }
+                res += 'b';
+            }
+    
+            return res;
+        }
+    };
