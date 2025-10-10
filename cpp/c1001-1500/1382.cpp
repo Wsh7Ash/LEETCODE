7da57dadb574a8911d09af3e4eed5fa569d00912
GWH <159546902+wahshf@users.noreply.github.com>
2025-10-10 18:19:32 +0300

    1382

---
+++ cpp/c1001-1500/1382.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    public:
+        TreeNode* balanceBST(TreeNode* root) {
+            vector<int> nodes;
+            inOrderTraversal(root, nodes);
+            return buildBalancedBST(nodes, 0, nodes.size() - 1);
+        }
+    
+    private:
+        void inOrderTraversal(TreeNode* node, vector<int>& nodes){
+            if(!node) return;
+    
+            inOrderTraversal(node->left, nodes);
+            nodes.push_back(node->val);
+            inOrderTraversal(node->right, nodes);
+        }
+    
+        TreeNode* buildBalancedBST(vector<int>& nodes, int left, int right){
+            if(left > right) return nullptr;
+    
+            int mid = left + (right - left) / 2;
+    
+            TreeNode* root = new TreeNode(nodes[mid]);
+    
+            root->left = buildBalancedBST(nodes, left, mid - 1);
+            root->right = buildBalancedBST(nodes, mid + 1, right);
+    
+            return root;
+        }
+    };
