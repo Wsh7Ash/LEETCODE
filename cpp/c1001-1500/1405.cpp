84be09f7cd6bd1e48325fab4a820b274248f823b
GWH <159546902+wahshf@users.noreply.github.com>
2025-10-11 19:20:59 +0300

    1405

---
+++ cpp/c1001-1500/1405.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    public:
+        string longestDiverseString(int a, int b, int c) {
+            priority_queue<pair<int, char>> pq;
+            if(a > 0) pq.push({a, 'a'});
+            if(b > 0) pq.push({b, 'b'});
+            if(c > 0) pq.push({c, 'c'});
+        
+            string res = "";
+    
+            while(!pq.empty()){
+                auto [count1, char1] = pq.top();
+                pq.pop();
+    
+                if(res.size() >= 2 && res[res.size() - 1] == char1 && res[res.size() - 2] == char1){
+                    if(pq.empty()) break;
+    
+                    auto[count2, char2] = pq.top();
+                    pq.pop();
+    
+                    res += char2;
+                    count2--;
+    
+                    if(count2 > 0) pq.push({count2, char2});
+                    pq.push({count1, char1});
+                }else{
+                    res += char1;
+                    count1--;
+                    if(count1 > 0) pq.push({count1, char1});
+                }
+            }
+            return res;
+        }
+    };
