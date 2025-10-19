582856f8329382c57ed096e11056196a2da5f77a
GWH <159546902+wahshf@users.noreply.github.com>
2025-10-19 19:57:25 +0300

    1560

---
+++ cpp/1501-2000/1560.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    public:
+        vector<int> mostVisited(int n, vector<int>& rounds) {
+            int start = rounds[0];
+            int end = rounds.back();
+            vector<int> res;
+    
+            if(start <= end){
+                for(int i = start; i <= end; i++){
+                    res.push_back(i);
+                }
+            }else{
+                for(int i = 1; i <= end; i++){
+                    res.push_back(i);
+                }
+                for(int i = start; i <= n; i++){
+                    res.push_back(i);
+                }
+            }
+            return res;
+        }
+    };
