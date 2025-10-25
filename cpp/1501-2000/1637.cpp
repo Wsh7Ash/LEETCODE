d024c8dcaf0e50576b74c6899ed9fbfef62775c3
GWH <159546902+wahshf@users.noreply.github.com>
2025-10-25 14:24:39 +0300

    1637

---
+++ cpp/1501-2000/1637.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public:
+        int maxWidthOfVerticalArea(vector<vector<int>>& points) {
+            sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b){
+                return a[0] < b[0];
+            });
+            int maxGap = 0;
+            for(int i = 1; i < points.size(); i++){
+                maxGap = max(maxGap, points[i][0] - points[i - 1][0]);
+            }
+            return maxGap;
+        }
+    };
