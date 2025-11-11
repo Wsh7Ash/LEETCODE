dddd7183dda4f63071d2235c02ea0a62d55048ee
GWH <159546902+wahshf@users.noreply.github.com>
2025-11-11 18:42:52 +0300

    1696

---
+++ cpp/1501-2000/1696.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    public:
+        int maxResult(vector<int>& nums, int k) {
+            int n = nums.size();
+            vector<int> dp(n, 0);
+            dp[0] = nums[0];
+    
+            deque<int> dq;
+            dq.push_back(0);
+    
+            for(int i = 1; i < n; i++){
+                while(!dq.empty() && dq.front() < i - k){
+                    dq.pop_front();
+                }
+                dp[i] = dp[dq.front()] + nums[i];
+                while(!dq.empty() && dp[dq.back()] <= dp[i]){
+                    dq.pop_back();
+                }
+                dq.push_back(i);
+            }
+            return dp[n - 1];
+        }
+    };
